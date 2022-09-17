dd50c4cd983a5b7609ef00b3f52884b0ff351c04
Zoraez <82158378+ZI-Youtuber@users.noreply.github.com>
2022-09-17 11:39:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// Baby Weather App
+//   if raining , 'its raining'
+//   else 'its a normal day today'
+
+let weather = prompt('How is the weather today ?')
+  if(weather == 'rain'){
+    console.log('wear rain jacket to not get wet')
+  } else {console.log('wear your sunglasses')
+  }
